b67fe26edcb163bf0ae5e06735cbaa0d3759e0e6
Zach Stence <zachstence@gmail.com>
2018-10-26 13:21:35 -0500

    Finish project
    - Change interpreter to 2.7 from 3.5.6
    - Make most functions private
    - Add getters for registers and memory
    - Add exception catch when trying to access instruction out of range
    - Add docstrings
    - Refactor function names
    - Add command line argument parsing

--- team13_project2.py
+++ team13_project2.py
@@ -1,10 +1,5 @@
+import sys
 from Disassembler import Disassembler
-from pprint import pprint
-
-# CHECK TO MAKE SURE BRANCH INSTRUCTIONS DON'T BRANCH OUTSIDE OF INSTRUCTION MEMORY
-# Display memory starting right after instruction memory
-# should dynamically display memory based off of where stuff is added
-# display empty memory (all memory from after instructions to end of non-zero)
 
 
 class Simulator:
@@ -18,24 +13,35 @@ class Simulator:
         self.__pc = 96
 
     def run(self):
+        """
+        Calls all necessary functions to simulate the code.
+        """
         type = ''
         out = open(self.__output_file + '_sim.txt', 'w')
         while type != 'BREAK':
-            inst = self.__instructions[self.__pc]
+            try:
+                inst = self.__instructions[self.__pc]
+            except KeyError:
+                print >> sys.stderr, "ERROR: Can't access instruction outside instruction memory ({})".format(self.__pc)
+                quit(1)
             type = inst['type']
             f = getattr(self, '_Simulator__simulate_' + type.lower())
             f(inst)
-            out.write(self.get_sim_str(inst))
+            out.write(self.__get_sim_str(inst))
 
     def __simulate_r(self, inst):
+        """
+        Simulates an R-format instruction by performing various arithmetic.
+        :param inst: A dictionary containing the the instruction's information.
+        """
         name = inst['name']
         rm = inst['rm']
         shamt = inst['shamt']
         rn = inst['rn']
         rd = inst['rd']
 
-        rm_val = self.read_register(rm)
-        rn_val = self.read_register(rn)
+        rm_val = self.__read_register(rm)
+        rn_val = self.__read_register(rn)
 
         op_dict = {
             'AND': rm_val & rn_val,
@@ -48,50 +54,64 @@ class Simulator:
             'LSL': rm_val << shamt
         }
         rd_val = op_dict[name]
-        self.write_register(rd, rd_val)
+        self.__write_register(rd, rd_val)
 
         self.__pc += 4
 
     def __simulate_d(self, inst):
+        """
+        Simulates an D-format instruction by performing various arithmetic.
+        :param inst: A dictionary containing the the instruction's information.
+        """
         name = inst['name']
         offset = inst['offset']
         rn = inst['rn']
         rt = inst['rt']
 
-        rn_val = self.read_register(rn)
+        rn_val = self.__read_register(rn)
         address = rn_val + 4 * offset
 
         if name == 'STUR':
-            rt_val = self.read_register(rt)
-            self.write_memory(address, rt_val)
+            rt_val = self.__read_register(rt)
+            self.__write_memory(address, rt_val)
         elif name == 'LDUR':
-            rt_val = self.read_memory(address)
-            self.write_register(rt, rt_val)
+            rt_val = self.__read_memory(address)
+            self.__write_register(rt, rt_val)
 
         self.__pc += 4
 
     def __simulate_i(self, inst):
+        """
+        Simulates an I-format instruction by performing various arithmetic.
+        :param inst: A dictionary containing the the instruction's information.
+        """
         name = inst['name']
         immediate = inst['immediate']
         rn = inst['rn']
         rd = inst['rd']
-        rn_val = self.read_register(rn)
+        rn_val = self.__read_register(rn)
         if name == 'ADDI':
             rd_val = rn_val + immediate
         elif name == 'SUBI':
             rd_val = rn_val - immediate
 
-        self.write_register(rd, rd_val)
+        self.__write_register(rd, rd_val)
         self.__pc += 4
 
     def __simulate_b(self, inst):
-        # opcode = inst['opcode']
+        """
+        Simulates an B-format instruction by performing various arithmetic.
+        :param inst: A dictionary containing the the instruction's information.
+        """
         offset = inst['offset']
         self.__pc += offset * 4
 
     def __simulate_cb(self, inst):
+        """
+        Simulates an CB-format instruction by performing various arithmetic.
+        :param inst: A dictionary containing the the instruction's information.
+        """
         name = inst['name']
-        # opcode = inst['opcode']
         offset = inst['offset']
         rt = inst['rt']
         if (name == 'CBZ' and rt == 0) or (name == 'CBNZ' and rt != 0):
@@ -100,6 +120,10 @@ class Simulator:
             self.__pc += 4
 
     def __simulate_im(self, inst):
+        """
+        Simulates an IM-format instruction by performing various arithmetic.
+        :param inst: A dictionary containing the the instruction's information.
+        """
         name = inst['name']
         shift = inst['shift']
         immediate = inst['immediate']
@@ -107,43 +131,94 @@ class Simulator:
 
         val = immediate << (shift * 16)
         if name == 'MOVZ':
-            self.write_register(rd, val)
+            self.__write_register(rd, val)
         elif name == 'MOVK':
-            rd_val = self.read_register(rd)
+            rd_val = self.__read_register(rd)
             mask = 0xFFFF << (shift * 16)
             rd_val = (rd_val & mask) | val
-            self.write_register(rd, rd_val)
+            self.__write_register(rd, rd_val)
 
         self.__pc += 4
 
     def __simulate_nop(self, inst):
+        """
+        Simulates an NOP-format instruction by performing various arithmetic.
+        :param inst: A dictionary containing the the instruction's information.
+        """
         self.__pc += 4
 
     def __simulate_break(self, inst):
+        """
+        Simulates an BREAK-format instruction by performing various arithmetic.
+        :param inst: A dictionary containing the the instruction's information.
+        """
         self.__pc += 4
         pass
 
-    def read_register(self, r):
+    def __read_register(self, r):
+        """
+        Reads and returns the value in a specified register.
+        :param r: The register (0-31).
+        :return: The value in register r.
+        """
         return self.__registers[r]
 
-    def write_register(self, r, val):
+    def __write_register(self, r, val):
+        """
+        Writes a value to a specified register.
+        :param r: The register (0-31).
+        :param val: The value to write to register r.
+        """
         self.__registers[r] = val
 
-    def read_memory(self, a):
+    def __read_memory(self, a):
+        """
+        Reads and returns the value in a specified memory location.
+        :param a: The memory address.
+        :return: The value in memory address a.
+        """
         return self.__memory[a]
 
-    def write_memory(self, a, val):
+    def __write_memory(self, a, val):
+        """
+        Writes a value to a specified memory location.
+        :param a: The memory address.
+        :param val: The value to write to memory address a.
+        """
         self.__memory[a] = val
 
-    def get_sim_str(self, inst):
+    def __get_sim_str(self, inst):
+        """
+        Returns a string to be printed to the command line for tracking state of the simulator.
+        :param inst: A dictionary containing the isntruction's information.
+        :return:
+        """
         out = '=' * 21 + '\n' \
                + 'cycle:{}\t{}\t{}\n'.format(int((self.__pc - 96) / 4), inst['address'], inst['assembly']) + '\n' \
-               + self.print_registers() \
+               + self.registers_to_string() \
                + '\n' \
-               + self.print_memory()
+               + self.memory_to_string()
         return out
 
-    def print_registers(self):
+    def get_registers(self):
+        """
+        Returns the current registers.
+        :return The simulator's registers.
+        """
+        return self.__registers
+
+    def get_memory(self):
+        """
+        Returns the current memory.
+        :return: The simulator's memory.
+        """
+        return self.__memory
+
+    def registers_to_string(self):
+        """
+        Returns a string of the 32 registers, 4 lines with 8 each and a label for each line.
+        :return: A string representation of the registers.
+        """
         out = 'registers:\n'
         for i in range(4):
             out += 'r{:02d}:'.format(i*8)
@@ -152,7 +227,11 @@ class Simulator:
             out += '\n'
         return out
 
-    def print_memory(self):
+    def memory_to_string(self):
+        """
+        Returns a string of the memory, with a label for each line.
+        :return: A string representation of the memory.
+        """
         out = 'data:'
 
         # Fix stupid formatting, thanks Greg
@@ -182,8 +261,15 @@ class Simulator:
 
 
 if __name__ == '__main__':
-    infile = 'test11_bin.txt'
-    outfile = 'team13_out'
+    infile = ''
+    outfile = ''
+
+    # Get in/out file from command line arguments
+    for i in range(len(sys.argv)):
+        if sys.argv[i] == '-i':
+            infile = sys.argv[i + 1]
+        elif sys.argv[i] == '-o':
+            outfile = sys.argv[i + 1]
 
     d = Disassembler(infile, outfile)
     d.run()
